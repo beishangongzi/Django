008214dbcce24c4ecfdfa06814d0909ed70ecdd3
andy <zhangruibin021@gmail.com>
2021-08-11 12:11:00 +0800

    change database to mysql

--- mblog/mblog/settings.py
+++ mblog/mblog/settings.py
@@ -81,8 +81,10 @@ WSGI_APPLICATION = 'mblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "mblog",
+        'PASSWORD': 'andy123',
+        'HOST': '10.214.3.168',
     }
 }
 
